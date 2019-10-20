0db105b013020490ab01faa4f676ea8a59ce3eb7
Michael Miranda <42720791+mrpineapples@users.noreply.github.com>
2019-10-20 18:30:13 -0400

    feat: adds filter function and tests it

--- card.go
+++ card.go
@@ -114,3 +114,15 @@ func Jokers(n int) func([]Card) []Card {
 		return cards
 	}
 }
+
+func Filter(f func(card Card) bool) func([]Card) []Card {
+	return func(cards []Card) []Card {
+		var ret []Card
+		for _, card := range cards {
+			if !f(card) {
+				ret = append(ret, card)
+			}
+		}
+		return ret
+	}
+}
--- card_test.go
+++ card_test.go
@@ -56,3 +56,15 @@ func TestJokers(t *testing.T) {
 		t.Error("Expected 3 Jokers, receieved:", count)
 	}
 }
+
+func TestFilter(t *testing.T) {
+	filter := func(card Card) bool {
+		return card.Rank == Two || card.Rank == Three
+	}
+	cards := New(Filter(filter))
+	for _, card := range cards {
+		if card.Rank == Two || card.Rank == Three {
+			t.Error("Expected all twos and threes to be filtered out.")
+		}
+	}
+}
